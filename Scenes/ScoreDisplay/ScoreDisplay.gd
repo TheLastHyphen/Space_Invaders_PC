754d7f5516a84703693f7c9c9021bd5ae51a6441
David Nicholas <davidnic@live.co.uk>
2026-08-31 19:44:33 +0100

    Added ability to flash the score. This was observed in the original game. It originally was used to indicate which player's turn it was.
    Added function to set the initial high score. This is used to set the high score after it has been loaded.

--- Scenes/ScoreDisplay/ScoreDisplay.gd
+++ Scenes/ScoreDisplay/ScoreDisplay.gd
@@ -5,16 +5,27 @@ extends Control
 
 func _ready() -> void:
 	hi_score_label.text = "%04d" % HighScore.HighScore
+	var n: StringName = get_tree().get_current_scene().name
+	if n != "Main":
+		FlashScore()
 
 func ClearScore() -> void:
 	score_label.text = "%04d" % 0
+	hi_score_label.text = "%04d" % 0
 
 func UpdateScores(score: int) -> void:
 	score_label.text = "%04d" % score
 	if score > HighScore.HighScore:
 		UpdateHighScore(score)
-		#HighScore.HighScore = highScore
-	
+
 func UpdateHighScore(score: int) -> void:
 	HighScore.HighScore = score
 	hi_score_label.text = "%04d" % HighScore.HighScore
+
+func SetInitialHighScore() -> void:
+	UpdateHighScore(HighScore.HighScore)
+
+func FlashScore() -> void:
+	for i in range(44):
+		score_label.visible = !score_label.visible
+		await  get_tree().create_timer(0.04).timeout
