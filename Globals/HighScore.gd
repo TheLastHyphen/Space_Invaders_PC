1ba195c015a50273a9edab5b78ed84b1d279a040
David Nicholas <davidnic@live.co.uk>
2026-08-31 19:40:02 +0100

    Added load and save high score. This makes the highs score persistent between games.

--- Globals/HighScore.gd
+++ Globals/HighScore.gd
@@ -1,6 +1,21 @@
 extends Node
 
+const SAVE_PATH: String = "user://space_invaders_high_score.dat"
 
 var HighScore: int:
 	get: return HighScore
 	set(value): HighScore = value
+
+func SaveHighScore() -> void:
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	if !file:
+		push_error("save_to_file no file found!")
+		return
+	file.store_32(HighScore)
+
+func LoadHiScore() -> void:
+	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	if !file:
+		push_error("load_from_file no file found!")
+		return
+	HighScore = file.get_32()
