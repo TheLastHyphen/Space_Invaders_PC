62e7feb6e59335491e76138aa87b983563670f80
David Nicholas <davidnic@live.co.uk>
2026-09-02 20:57:21 +0100

    Added functionality to emulate the player movement and firing the missile. Player movement is based on a random time. The player is simulated to move either left or right. Once the timer timeouts the direction is swapped.

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -5,6 +5,7 @@ extends Area2D
 @onready var player_base: Sprite2D = $PlayerBase
 @onready var player_explosion: AnimatedSprite2D = $PlayerExplosion
 @onready var player_fire: AudioStreamPlayer2D = $PlayerFire
+@onready var demo_move_timer: Timer = $DemoMoveTimer
 
 const PLAYER_MISSILE = preload("uid://vy0it3bcev22")
 
@@ -12,20 +13,62 @@ var canMove: bool = true
 var canFire: bool = true
 var missile: Area2D
 
+var moveLeft: bool = false
+var moveRight: bool = false
+var flip: bool = false
+var demoFire: bool = false
+
 func _ready() -> void:
 	player_explosion.hide()
 	SignalHub.OnPlayerHit.connect(OnPlayerHit)
+	if States.isDemo == true:
+		demo_move_timer.start()
+		demoFire = true
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("PlayerFire") && canFire:
-		if missile == null:
-			missile = PLAYER_MISSILE.instantiate()
-			missile.position = position
-			get_parent().add_child(missile)
+func FirePlayerMissile() -> void:
+	if missile == null:
+		missile = PLAYER_MISSILE.instantiate()
+		missile.position = position
+		get_parent().add_child(missile)
+		if States.isDemo == false:
 			player_fire.play()
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_type() && States.isDemo:
+		States.isDemo = false
+		GameManager.LoadMain()
+	if demoFire == true && States.isDemo:
+		FirePlayerMissile()
+		demoFire = false
+	
+	if event.is_action_pressed("PlayerFire") && canFire && States.isDemo == false:
+		FirePlayerMissile()
+		#if missile == null:
+			#missile = PLAYER_MISSILE.instantiate()
+			#missile.position = position
+			#get_parent().add_child(missile)
+			#if States.isDemo == false:
+				#player_fire.play()
+
 func _process(delta: float) -> void:
-	if canMove:
+	if States.isDemo == true:
+		if canMove:
+			if demoFire: FirePlayerMissile()
+			if moveLeft:
+				if position.x > 64.0:
+					position.x -= delta * playerSpeed
+				else:
+					moveLeft = false
+					moveRight = true
+			
+			if moveRight:
+				if position.x < 1070:
+					position.x += delta * playerSpeed
+				else:
+					moveLeft = true
+					moveRight = false
+	
+	if canMove && States.isDemo == false:
 		var vpr: Rect2 = get_viewport_rect()
 		var dir: float = Input.get_axis("PlayerLeft", "PlayerRight")
 		position.x += dir * delta * playerSpeed
@@ -49,3 +92,17 @@ func _on_player_explosion_finished() -> void:
 	player_base.show()
 	canMove = true
 	canFire = true
+
+func _on_demo_move_timer_timeout() -> void:
+	demo_move_timer.stop()
+	var random = randf_range(0.25, 2)
+	demo_move_timer.wait_time = random
+	if flip == true:
+		moveRight = true
+		moveLeft = false
+	if flip == false:
+		moveLeft = true
+		moveRight = false
+	flip = !flip
+	demoFire = true
+	demo_move_timer.start()
